9770f1bb92608bb246088eebcc2634e9b7f9510a
LeonrdoBatistaCarias <leonardo1carias@gmail.com>
2019-12-14 18:53:09 -0300

    Configurando o Multer e criando avatar do usuario

---
+++ src/database/migrations/20191214203803-add-avatar-field-to-users.js
@@ -0,0 +1,20 @@
+/* eslint-disable strict */
+/* eslint-disable arrow-body-style */
+
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
