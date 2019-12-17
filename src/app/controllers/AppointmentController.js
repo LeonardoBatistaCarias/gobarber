09623b167efaccae18c4ef60a21e9aa225cf229f
LeonrdoBatistaCarias <leonardo1carias@gmail.com>
2019-12-16 21:12:41 -0300

    Listando agenda do provider, configuração do mongo e notificação de novos agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+// eslint-disable-next-line object-curly-newline
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -83,6 +86,24 @@ class AppointmentController {
       date,
     });
 
+    /**
+     * Notify appointment provider
+     */
+
+    const user = await User.findByPk(req.userId);
+
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' HH:mm'h'",
+      {
+        locale: 'pt',
+      },
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
+    });
     return res.json(appointment);
   }
 }
