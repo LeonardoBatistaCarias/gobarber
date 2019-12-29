e1cfc6dad2c9f822ccc483bb713ee687c45212ec
LeonardoBatistaCarias <leonardo1carias@gmail.com>
2019-12-28 21:08:00 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3f2b12742dc2c3',
+    pass: '9ecde3ad92196d',
+  },
+  default: {
+    from: 'Leonardo Batista Carias <noreply@gmail.com>',
+  },
+
+};
